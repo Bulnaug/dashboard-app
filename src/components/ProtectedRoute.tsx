6aa4a81491e2cf331f804f1f609b86698657819c
Bulnaug <basalay.1997@mail.ru>
2025-12-27 01:59:26 +0100

    vercel update

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,7 +1,8 @@
 import { Navigate } from "react-router-dom"
 import { useAuth } from "../context/AuthContext"
+import { type ReactNode } from "react"
 
-const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
+const ProtectedRoute = ({ children }: { children: ReactNode }) => {
   const { session, loading } = useAuth()
 
   if (loading) return <div>Loading...</div>
